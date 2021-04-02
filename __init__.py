d4282e4046d38ec9b0f0ef5710493276baee5526
Emma Chickles <echickle@wellesley.edu>
2021-04-02 16:17:20 -0400

    init

--- __init__.py
+++ __init__.py
@@ -1,5 +1,15 @@
+"""
+210402 - Emma Chickles
+"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import pdb # >> debug helper
+import os
+
+import sys
+sys.path.insert(0, '../mergen/mergen/')
+import mergen
+
---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+210402 - Emma Chickles
+"""
+
+from __init__ import *
+
+datapath = '/nfs/blender/data/tdaylan/data/'
+savepath = '/nfs/blender/data/tdaylan/data/'
+datatype = 'SPOC'
+mdumpcsv = datapath + 'Table_of_momentum_dumps.csv'
+
+mg = mergen.mergen(datapath, savepath, datatype, mdumpcsv)
