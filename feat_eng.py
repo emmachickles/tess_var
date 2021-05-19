6f96662d8e3cc50a39248f43d12ec67cc830c2a5
Emma Chickles <echickle@wellesley.edu>
2021-05-19 08:27:18 -0400

    phase curve feature testing

--- feat_eng.py
+++ feat_eng.py
@@ -12,63 +12,70 @@
 
 from __init__ import *
 
-def create_phase_curve_feats(time, flux, n_bins=100, n_freq=500000, 
-                             n_terms=1, sector=1, output_dir='./'):
+def create_phase_curve_feats(time, flux, ids, n_bins=100, n_freq=500000, 
+                             n_terms=1, n_freq0=10000, n_terms0=1,
+                             sector=1, output_dir='./', plot=False,
+                             report_time=False):
 
-    feats = np.empty((len(flux), n_bins))
+    # >> textfile showing TICIDs where phase curve features were computed
+    fname = output_dir+'Sector'+str(sector)+'_phase_curve_feat_gen.txt'
+    with open(fname, 'w') as f: 
+        f.write('')
 
+    all_feats = []
     # >> calculate phase curves
     for i in range(len(flux)):
-        feats[i] = calc_phase_curve(time, flux[i], n_bins=n_bins, n_freq=n_freq,
-                                    n_terms=n_terms)
-        pdb.set_trace()
+        # >> check if light curve is periodic
+        res = mask_harmonics(time, flux[i], n_freq=n_freq0, n_terms=n_terms0,
+                             report_time=report_time, plot=plot, output_dir=output_dir,
+                             prefix='TIC'+str(ids[i]))
+
+        feats = calc_phase_curve(time, flux[i], n_bins=n_bins, n_freq=n_freq,
+                                 n_terms=n_terms, report_time=report_time, plot=plot,
+                                 output_dir=output_dir, prefix='TIC'+str(ids[i]))
+        with open(fname, 'a') as f:
+            f.write(str(ids[i])+','+str(type(feats)==type(None)))
+        if not type(feats) == type(None):
+            all_feats.append(feats)
 
     feats = dt.standardize(feats)
+    np.savetxt(output_dir+'Sector'+str(sector)+'_phase_curve_feat.txt',
+               np.array(feats))
 
-    if not os.path.exists(output_dir+'pcfeat/'):
-        os.makedirs(output_dir+'pcfeat/')
-    hdr = fits.Header()
-    hdu = fits.PrimaryHDU(feats, header=hdr)
-    hdu.writeto(output_dir+'pcfeat/Sector'+str(sector)+'-pcfeat.fits')
+def mask_harmonics(t, y, n_freq=10000, n_terms=1, thresh_min=5e-3, thresh_max=5e-2,
+                   tmin=0.05, tmax=5, har_window=100, kernel_size=25,
+                   report_time=True, plot=True, output_dir='', prefix=''):
+    ''' Mask harmonics of the largest power to determine whether it is
+    appropriate to compute phase curve features. Should take ~1 ms for short-
+    cadence light curves.
 
-def calc_phase_curve(t, y, n_bins=100, n_freq=50000, n_terms=2,
-                     har_window=100, kernel_size=25,
-                     plot=False, output_dir='', prefix='', report_time=True,
-                     n_freq0=10000, frac=20, frac_max=1e-3,
-                     tmin=0.05, tmax=27, tmax0=5):
-
-    # >> temporal baseline is 27 days, and shortest timescale sensitive to is
-    # >> 4 minutes
-    #tmax = 27 # days
-    # tmin = 4./1440 # days
-    #tmin = 0.025
+    thresh_min : minimum threshold power for a peak (used to determine the width
+                 of maximum peak)
+    thresh_max : maximum threshold power for a peak (used to determine if there is
+                 more than one signficiant component in periodogram'''
 
     from astropy.timeseries import LombScargle
     from astropy.timeseries import TimeSeries
     from astropy.time import Time
+    from scipy.signal import medfilt
     import astropy.units as u
 
-    if report_time:
+    if report_time: # >> restart timer
         from datetime import datetime
         start = datetime.now()
 
-    # -- check whether to do phase curve ---------------------------------------
-    # >> first use sparse freq grid
-    frequency = np.linspace(1./tmax0, 1./tmin, n_freq0)
+    # -- calculate a sparse periodogram ----------------------------------------
+    frequency = np.linspace(1./tmax, 1./tmin, n_freq)
     power = LombScargle(t, y, nterms=n_terms).power(frequency)    
-
     max_pow = np.max(power)
     max_ind = np.argmax(power)
     max_freq = frequency[max_ind] 
     
-    # >> remove harmonics
+    # -- mask harmonics --------------------------------------------------------
     factors = np.arange(2,6)
     windows = []
     for factor in factors:
         har_ind = np.argmin(np.abs(frequency - max_freq/factor))
-        # har_window = 300
-        # har_window = har_wind * 10**frequency[har_ind]
-        # har_window = har_wind * frequency[har_ind]
         window = np.arange(np.max([0,har_ind-har_window]),
                            np.min([har_ind+har_window, len(frequency)]),
                            dtype='int')
@@ -76,24 +83,18 @@ def calc_phase_curve(t, y, n_bins=100, n_freq=50000, n_terms=2,
 
         har_ind = np.argmin(np.abs(frequency - max_freq*factor))
                 
-        # har_window = har_wind * frequency[har_ind]
         window = np.arange(np.max([0,har_ind-har_window]),
                            np.min([har_ind+har_window, len(frequency)]),
                            dtype='int')
         windows.append(window)
+
     # >> frequency inds corresponding to harmonics
     inds = np.array([i for wind in windows for i in wind]).astype('int')
 
-    # >> add max peak
-    # max_window = max_wind * frequency[max_wind]
-    # window = np.arange(np.max([0,max_ind-max_window]),
-    #                    np.min([max_ind+max_window, len(frequency)]))
-
-    # >> find width
-    from scipy.signal import medfilt
+    # -- mask maximum peak -----------------------------------------------------
+    # >> find width of maximum peak
     smoothed_power = medfilt(power, kernel_size=kernel_size)
-    # no_peak_inds = np.nonzero((power-1e-3*np.max(power))<0)[0]
-    no_peak_inds = np.nonzero((smoothed_power-frac_max*np.max(smoothed_power))<0)[0]
+    no_peak_inds = np.nonzero((smoothed_power-thresh_min*np.max(smoothed_power))<0)[0]
     sorted_inds = no_peak_inds[np.argsort(np.abs(no_peak_inds - max_ind))]
     if max_ind < np.min(sorted_inds):
         left_ind = 0
@@ -106,11 +107,17 @@ def calc_phase_curve(t, y, n_bins=100, n_freq=50000, n_terms=2,
     window = np.arange(left_ind, right_ind)
     windows.append(window)
     inds = np.append(inds, window)
-    # inds = np.append(inds, np.arange(0, np.argmin(frequency-mask_freq)))
+
+    # -- plot ------------------------------------------------------------------
+
+    if report_time:
+        end = datetime.now()
+        dur_sec = (end-start).total_seconds()
+        print('Time to find second largest component: '+str(dur_sec))
     
     if plot:
         fig, ax = plt.subplots(2, figsize=(8, 2*3))
-        ax[0].set_title('Sparse frequency grid (nfreq0='+str(n_freq0)+')') 
+        ax[0].set_title('Sparse frequency grid (nfreq0='+str(n_freq)+')') 
         ax[0].plot(t, y, '.k', ms=1)
         ax[0].set_xlabel('Time [BJD - 2457000]')
         ax[0].set_ylabel('Relative Flux')
@@ -145,60 +152,90 @@ def calc_phase_curve(t, y, n_bins=100, n_freq=50000, n_terms=2,
                        fontsize='small')
         
         fig.tight_layout()
-        fig.savefig(output_dir+prefix+'sparse_pgram.png')
+        fig.savefig(output_dir+prefix+'nfreq'+str(n_freq)+'-nterms'+\
+                    str(n_terms)+'sparse_pgram.png')
         print('Saved '+output_dir+prefix+'sparse_pgram.png')
         plt.close(fig)
 
-    # >> look for a peak that isn't a harmonic
-    if np.max(np.delete(power, inds)) > max_pow/frac:
+    # -- return result ---------------------------------------------------------
+    
+    if np.max(np.delete(power, inds)) > max_pow/thresh_max:
         print('Not making phase curve...')
-        return
+        return False
     else:
+        return True
 
-        # -- make phase curve --------------------------------------------------
-        frequency = np.linspace(1./tmax, 1./tmin, n_freq)
-        power = LombScargle(t, y, nterms=n_terms).power(frequency)
 
-        best_freq = frequency[np.argmax(power)] 
-        period = 1/best_freq
+def calc_phase_curve(t, y, n_bins=100, n_freq=50000, n_terms=2,
+                     plot=False, output_dir='', prefix='', report_time=True,
+                     tmin=0.05, tmax=27):
+    '''frac_max : adjusts the threshold power to determine the width of maximum peak
+    frac : adjusts the threshold power to determine the signfiicance of second
+    largest peak'''
 
-        time = Time(t, format='jd')
-        ts = TimeSeries(time=time, data={'flux': y})
-        ts_folded = ts.fold(period=period*u.d) 
+    # >> temporal baseline is 27 days, and shortest timescale sensitive to is
+    # >> 4 minutes
+    #tmax = 27 # days
+    # tmin = 4./1440 # days
+    #tmin = 0.025
 
+    from astropy.timeseries import LombScargle
+    from astropy.timeseries import TimeSeries
+    from astropy.time import Time
+    import astropy.units as u
+
+    if report_time: # >> restart timer
+        from datetime import datetime
+        start = datetime.now()
+
+    # -- compute periodogram ---------------------------------------------------
+    frequency = np.linspace(1./tmax, 1./tmin, n_freq)
+    power = LombScargle(t, y, nterms=n_terms).power(frequency)
+
+    best_freq = frequency[np.argmax(power)] 
+    period = 1/best_freq
+
+    # -- compute phase curve ---------------------------------------------------
+    time = Time(t, format='jd')
+    ts = TimeSeries(time=time, data={'flux': y})
+    ts_folded = ts.fold(period=period*u.d) 
+
+    if report_time:
+        end = datetime.now()
+        dur_sec = (end-start).total_seconds()
+        print('Time to make phase curve: '+str(dur_sec))
+
+    # >> bin phase curve
+    orig_len = ts_folded['flux'].shape[0]
+    new_len = orig_len - orig_len%n_bins
+
+    folded = ts_folded['flux'][np.argsort(ts_folded.time.value)]
+    pc_feats = np.array(np.split(folded[:new_len], n_bins))
+    pc_feats = np.mean(pc_feats, axis=1)
+
+    if plot:
+        fig, ax = plt.subplots(4, figsize=(8, 4*3))
         if report_time:
-            end = datetime.now()
-            dur_sec = (end-start).total_seconds()
-            print('Time to make phase curve: '+str(dur_sec))
+            ax[0].set_title('Computation time: '+str(dur_sec)) 
+        ax[0].plot(t, y, '.k', ms=1)
+        ax[0].set_xlabel('Time [BJD - 2457000]')
+        ax[0].set_ylabel('Relative Flux')
+        ax[1].set_title('Period: '+str(np.round(period, 3))+' days')
+        ax[1].plot(frequency, power, '.k', ms=1)
+        ax[1].set_xlabel('Frequency')
+        ax[1].set_ylabel('Power')
+        # ax[1].set_xscale('log')
+        ax[2].plot(ts_folded.time.value, ts_folded['flux'], '.k', ms=1)
+        ax[2].set_xlabel('Time from midpoint epoch [days]')
+        ax[2].set_ylabel('Relative Flux')
+        ax[3].plot(np.arange(len(pc_feats)), pc_feats, '.')
+        ax[3].set_ylabel('Binned phase curve (nbins=' +str(n_bins)+ ')')
+        fig.tight_layout()
+        fig.savefig(output_dir+prefix+'phase_curve.png')
+        print('Saved '+output_dir+prefix+'nfreq'+str(n_freq)+'-nterms'+\
+              str(n_terms)+'-phase_curve.png')
+        plt.close(fig)
 
-        # >> bin phase curve
-        orig_len = ts_folded['flux'].shape[0]
-        new_len = orig_len - orig_len%n_bins
-        pc_feats = np.array(np.split(ts_folded['flux'][:new_len], n_bins))
-        pc_feats = np.mean(pc_feats, axis=1)
-
-        if plot:
-            fig, ax = plt.subplots(4, figsize=(8, 4*3))
-            if report_time:
-                ax[0].set_title('Computation time: '+str(dur_sec)) 
-            ax[0].plot(t, y, '.k', ms=1)
-            ax[0].set_xlabel('Time [BJD - 2457000]')
-            ax[0].set_ylabel('Relative Flux')
-            ax[1].set_title('Period: '+str(np.round(period, 3))+' days')
-            ax[1].plot(frequency, power, '.k', ms=1)
-            ax[1].set_xlabel('Frequency')
-            ax[1].set_ylabel('Power')
-            # ax[1].set_xscale('log')
-            ax[2].plot(ts_folded.time.value, ts_folded['flux'], '.k', ms=1)
-            ax[2].set_xlabel('Time from midpoint epoch [days]')
-            ax[2].set_ylabel('Relative Flux')
-            ax[3].plot(np.arange(len(pc_feats)), pc_feats, '.')
-            ax[3].set_ylabel('Binned phase curve (nbins=' +str(n_bins)+ ')')
-            fig.tight_layout()
-            fig.savefig(output_dir+prefix+'phase_curve.png')
-            print('Saved '+output_dir+prefix+'phase_curve.png')
-            plt.close(fig)
-
-        return pc_feats
+    return pc_feats
 
 
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import feat_eng as fe
 
 datapath = '/nfs/blender/data/tdaylan/data/'
 savepath = '/nfs/blender/data/tdaylan/Mergen_Run_2/'
+output_dir='/nfs/ger/home/echickle/'
 datatype = 'SPOC'
 mdumpcsv = datapath + 'Table_of_momentum_dumps.csv'
 sector   = 1
-n_freq   = 100000
-n_freq0  = 20000
+n_freq   = 50000
+n_freq0  = 10000
 n_terms  = 2
 n_bins   = 100
 kernel_size = 15
@@ -22,24 +23,29 @@ obj = mg.mergen(datapath, savepath, datatype, mdumpcsv, sector=sector)
 obj.load_lightcurves_local()
 
 # targ = [25078924, 31655792, 260162199, 144194304]
-targ = [31655792,32200310,53842685,117515123,126637765,141268467,141606986,144194304,147083089,161262226,200645730,200655763,206537272,206555954,211412634,231629787,235000060,238170857,259901124,260162199,261089147,261259291,263078276,267043786,279955276,294273900,348717439,355544723,369218857,412063998, 419635446, 441105436]
+# targ = [31655792,32200310,53842685,117515123,126637765,141268467,141606986,144194304,147083089,161262226,200645730,200655763,206537272,206555954,211412634,231629787,235000060,238170857,259901124,260162199,261089147,261259291,263078276,267043786,279955276,294273900,348717439,355544723,369218857,412063998, 419635446, 441105436]
 # targ = [141268467]
 # targ = [260162199, 144194304]
 
 # >> non-periodic
-# targ = [24695725, 25132999, 25133286, 25299762, 25300088, 29285387]
-for t in targ:
-    ind = np.nonzero(obj.ticid==t)
-    y = obj.intensities[ind][0]
+targ = [24695725, 25132999, 25133286, 25299762, 25300088, 29285387]
+# for t in targ:
+#     ind = np.nonzero(obj.ticid==t)
+#     y = obj.intensities[ind][0]
+#     prefix = 'TIC'+str(t)
 
-    prefix = 'TIC'+str(t)+'-nfreq'+str(n_freq)+'-nterms-'+str(n_terms)+'-'
-    fe.calc_phase_curve(obj.times, y, output_dir='/nfs/ger/home/echickle/',
-                        prefix=prefix, plot=True, n_freq=n_freq,
-                        n_terms=n_terms, n_bins=n_bins,
-                        kernel_size=kernel_size, frac_max=frac_max)
+#     res = fe.mask_harmonics(obj.times, y, n_freq=n_freq0,
+#                          report_time=True, plot=True, output_dir=output_dir,
+#                             prefix='TIC'+str(t))
 
-pdb.set_trace()
-fe.create_phase_curve_feats(obj.times, obj.intensities, sector=obj.sector,
+#     if res:
+#         feats = fe.calc_phase_curve(obj.times, y, n_bins=n_bins, n_freq=n_freq,
+#                                     n_terms=n_terms, report_time=True, plot=True,
+#                                     output_dir=output_dir, prefix='TIC'+str(t))
+
+# pdb.set_trace()
+fe.create_phase_curve_feats(obj.times, obj.intensities, obj.ticid,
+                            sector=obj.sector,
                             output_dir=obj.savepath, n_freq=n_freq,
                             n_terms=n_terms, n_bins=n_bins,
                             n_freq0=n_freq0)
