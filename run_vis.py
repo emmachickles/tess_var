0601c92def7f45d2b08480a7029a68672c49e434
Emma Chickles <echickle@wellesley.edu>
2021-09-03 15:12:13 -0400

    updated lof plot

--- run_vis.py
+++ run_vis.py
@@ -12,7 +12,6 @@ from __init__ import *
 datapath     = '/nfs/blender/data/tdaylan/data/'
 savepath     = '/nfs/blender/data/tdaylan/Mergen_Run_1/'
 datatype     = 'SPOC'
-output_dir   = './'
 sector       = 1
 runiter      = True
 numiter      = 1
@@ -30,27 +29,17 @@ mg.load_lightcurves_local()
 mg.load_learned_features()
 mg.load_true_otypes()
 mg.numerize_true_otypes()
+
 # mg.generate_clusters()
 mg.load_gmm_clusters()
+
 # mg.generate_predicted_otypes()
 mg.load_pred_otypes()
+
 mg.numerize_pred_otypes()
 
-# -- produce t-SNE -------------------------------------------------------------
-# X = lt.run_tsne(mg.features, n_components=ncomp, perplexity=perplxty,
-#                 savepath=mg.ensbpath)
-X = lt.load_tsne(mg.ensbpath)
+# mg.generate_tsne()
+# mg.produce_clustering_visualizations()
 
-# -- produce plots -------------------------------------------------------------
-# prefix = 'perplexity'+str(perplexity)+'_elev'+str(elev)+'_'
-
-# >> color with clustering results
-prefix = 'pred_'
-pt.plot_tsne(mg.feats, mg.numpot, X=X, output_dir=output_dir,
-             prefix=prefix, animate=anim, elev=elev, otypedict=mg.potd)
-
-# >> color with classifications from GCVS, SIMBAD, ASAS-SN
-prefix = 'true_'
-pt.plot_tsne(mg.feats, mg.numtot, X=X, output_dir=output_dir,
-             prefix=prefix, animate=anim, elev=elev, otypedict=mg.totd)
-
+mg.generate_novelty_scores()
+mg.produce_novelty_visualizations()
