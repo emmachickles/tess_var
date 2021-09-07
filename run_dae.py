2ce5de43b7b9fe88c43816612f0ee85e4a0fd500
emmachickles <echickle@wellesley.edu>
2021-09-07 09:19:22 -0400

    restructuring

--- run_dae.py
+++ run_dae.py
@@ -9,30 +9,20 @@ Script to train a deep autoencoder on LS periodograms.
 from __init__ import *
 
 # -- inputs --------------------------------------------------------------------
-datapath     = '/nfs/blender/data/tdaylan/data/'
-savepath     = '/nfs/blender/data/tdaylan/Mergen_Run_2/'
-parampath    = './daehyperparams.txt'
+datapath     = '/Users/emma/Documents/MIT/TESS/Data/'
+# datapath     = '/nfs/blender/data/tdaylan/data/'
+
+savepath     = '/Users/emma/Documents/MIT/TESS/Plots/Ensemble-Sector_1/DAE/'
+# savepath     = '/nfs/blender/data/tdaylan/Mergen_Run_2/'
+
+parampath    = datapath + 'daehyperparams.txt'
 datatype     = 'SPOC'
+featgen      = 'DAE'
 sector       = 1
 
 # -- initialize Mergen object --------------------------------------------------
 mg = mergen.mergen(datapath, savepath, datatype, sector=sector,
                    parampath=parampath)
 
-mg.load_lightcurves_local()
-mg.preprocess_dae()
-mg.load_features('DAE')
-# mg.generate_dae_features()
-
-mg.load_true_otypes()
-mg.numerize_true_otypes()
-
-mg.generate_clusters()
-mg.generate_predicted_otypes()
-mg.numerize_pred_otypes()
-
-mg.generate_tsne()
-mg.produce_clustering_visualizations()
-
-mg.generate_novelty_scores()
-mg.produce_novelty_visualizations()
+mg.load_features(featgen)
+mg.run_feature_analysis(featgen)
