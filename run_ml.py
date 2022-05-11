f044fd6dc185ae39c278498f21194857b9328bb1
Emma Chickles <echickle@submit21.mit.edu>
2022-05-11 12:00:36 -0400

    only use tensorflow in this script

---
+++ run_ml.py
@@ -0,0 +1,39 @@
+"""
+Created on 211105
+
+run_cae.py
+Script to train a convolutional autoencoder on LS periodograms.
+@author: Emma Chickles (emmachickles)
+"""
+
+from __init__ import *
+import feat_eng as fe
+import tensorflow as tf
+
+# -- inputs --------------------------------------------------------------------
+
+timescale    = 1 # >> number of sectors (1, 6, or 13)
+savepath     = '/scratch/echickle/timescale-'+str(timescale)+'sector/'
+dt.create_dir(savepath)
+
+datapath     = '/scratch/data/tess/lcur/spoc/'
+metapath     = '/scratch/data/tess/meta/'
+datatype     = 'SPOC'
+featgen      = 'CAE'
+clstrmeth    = 'gmm'
+mdumpcsv     = '/scratch/data/tess/meta/Table_of_momentum_dumps.csv'
+numclstr     = 300
+
+# -- initialize Mergen object --------------------------------------------------
+mg = mergen(datapath, savepath, datatype, metapath=metapath,
+            featgen=featgen, clstrmeth=clstrmeth,
+            mdumpcsv=mdumpcsv, numclstr=numclstr)
+
+fe.load_lspgram_fnames(mg, timescale=timescale)
+with tf.device('/GPU:0'):
+    mg.optimize_params()
+pdb.set_trace()
+mg.generate_features() # >> feature extraction by conv autoencoder
+
+pdb.set_trace()
+mg.generate_clusters()
