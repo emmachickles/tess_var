2c3a311046a38fdb7d2ff57438fe5b31fc4dc1c3
emmachickles <echickle@wellesley.edu>
2022-05-31 13:15:49 -0400

    tsne visualizations

--- run_cae.py
+++ run_cae.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 
 timescale    = 1 # >> number of sectors (1, 6, or 13)
 
-machine      = 'submit'
+machine      = 'uzay'
 
 if machine == 'uzay':
     savepath     = '/scratch/echickle/timescale-'+str(timescale)+'sector/'
@@ -44,6 +44,7 @@ featgen      = 'CAE'
 clstrmeth    = 'gmm'
 numclstr     = 300
 train_on_    = 'lspm'
+# train_on_ = 'unreg-lspm'
 
 # -- initialize Mergen object --------------------------------------------------
 mg = mergen(datapath, savepath, datatype,  parampath=parampath,
@@ -57,5 +58,54 @@ fe.load_lspgram_fnames(mg, timescale=timescale)
 # mg.save_ae_features(reconstruct=False)
 
 mg.load_features()
-mg.generate_tsne()
-mg.generate_clusters()
+# mg.generate_tsne()
+# mg.generate_clusters()
+
+mg.load_tsne()
+mg.load_gmm_clusters()
+# mg.load_true_otypes()
+# 
+# mg.numerize_otypes()
+# mg.produce_clustering_visualizations()
+# prefix = 'gmm300_'
+# pt.plot_tsne(mg.feats, mg.clstr, X=mg.tsne, output_dir=mg.featpath,
+#           prefix=prefix)
+
+# unique, counts = np.unique(mg.numtot, return_counts=True)
+# inds = np.nonzero(mg.numtot == np.argmax(counts))
+# mg.numtot[inds] = 3000
+
+# prefix = 'true_'
+# pt.plot_tsne(mg.feats, mg.numtot, X=mg.tsne, output_dir=mg.featpath,
+#           prefix=prefix)
+
+# prefix = 'inset_'
+# pt.plot_tsne(mg.feats, mg.clstr, X=mg.tsne, output_dir=mg.featpath+'imgs/',
+#              prefix=prefix, plot_insets=True, objid=mg.objid, 
+#              lcpath=mg.datapath+'clip/', 
+#              max_insets=300)
+
+
+# prefix='crot_'
+# # >> known complex rotators (sectors 1, 2)
+# # targets      = [38820496, 177309964, 206544316, 234295610, 289840928,
+# #                 425933644, 425937691] # >> sector 1 only
+# targets = [38820496, 177309964, 201789285, 206544316, 224283342,\
+#            234295610, 289840928, 332517282, 425933644, 425937691]
+# numcot = [] # >> numerized complex rotator ensemble object type
+# cotd = {3000: 'NONE', 1:'CROT'}
+# for ticid in mg.objid:
+#     if int(ticid) in targets:
+#         numcot.append(1)
+#     else:
+#         numcot.append(3000)
+# pt.plot_tsne(mg.feats, numcot, X=mg.tsne, output_dir=mg.featpath+'imgs/',
+#              prefix=prefix, otypedict=cotd, alpha =1,
+#              class_marker='x', class_ms=20, objid=mg.objid,
+#              plot_insets=True, lcpath=mg.datapath+'clip/', inset_label=1)
+ 
+for i in range(10):
+    prefix = 'zoom_'+str(i)+'_'
+    pt.plot_tsne(mg.feats, mg.clstr, X=mg.tsne, output_dir=mg.featpath+'imgs/',
+                 prefix=prefix, objid=mg.objid, 
+                 lcpath=mg.datapath+'clip/', zoom=True)
