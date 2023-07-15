dab68a268b5f62b487635a5062f4ef48aab2a83f
emmachickles <echickle@wellesley.edu>
2023-07-15 09:10:19 -0700

    local 230715

--- plot_functions.py
+++ plot_functions.py
@@ -1,10 +1,6 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-from tensorflow.keras.models import Model
-import tensorflow_probability as tfp
-import tensorflow as tf
 
 def plot_loss(history, mydir):
     # Extract loss history
@@ -21,9 +17,20 @@ def plot_loss(history, mydir):
     plt.legend()
     plt.savefig(mydir + 'loss.png')
 
+def tsne(latent_vectors, n_components=2):
+    from sklearn.manifold import TSNE
+    
+    # Perform t-SNE dimensionality reduction on the latent vectors
+    tsne = TSNE(n_components=n_components, random_state=42)
+    latent_tsne = tsne.fit_transform(latent_vectors)
+    
+    return latent_tsne
+    
+
 def plot_tsne(latent_vectors, mydir):
     """Create a scatter plot of the t-SNE latent space"""
-
+    from sklearn.manifold import TSNE
+    
     # Perform t-SNE dimensionality reduction on the latent vectors
     tsne = TSNE(n_components=2, random_state=42)
     latent_tsne = tsne.fit_transform(latent_vectors)
@@ -42,10 +49,12 @@ def plot_tsne(latent_vectors, mydir):
 def plot_latent_images(decoder, n, latent_dim, latent_vectors, mydir):
     """Plots n x n decoded images sampled from the latent space and saves the figure to mydir."""
     import tensorflow_probability as tfp
+    import tensorflow as tf
+    from sklearn.manifold import TSNE
 
     norm = tfp.distributions.Normal(0, 1)
     grid_points = norm.quantile(np.linspace(0.05, 0.95, n))
-    grid_points = tf.reshape(grid_points, (n, 1))
+    grid_points = np.reshape(grid_points, (n, 1))
     z = tf.repeat(grid_points, repeats=latent_dim, axis=1)  # Repeat grid_points for each latent dimension
     x_decoded = decoder.predict(z, steps=1)  # Generate decoded images
 
@@ -163,6 +172,8 @@ def plot_reconstruction_distribution(original_data, reconstructed_data, mydir):
     plt.savefig(os.path.join(mydir, 'loss_dist.png'))
 
 def visualize_layer_outputs(model, data, num_examples, mydir):
+    from tensorflow.keras.moddels import Model
+    
     layer_names = [layer.name for layer in model.layers[1:]]  # Get the names of all layers in the model
     layer_outputs = [layer.output for layer in model.layers[1:]]  # Get the output tensors of all layers
 
@@ -200,13 +211,7 @@ def visualize_layer_outputs(model, data, num_examples, mydir):
             plt.savefig(os.path.join(mydir, f'{num}_{layer_name}.png'))
             plt.close()
 
-def plot_cluster(latent_vectors, mydir, cluster_method='dbscan', n_clusters=35):
-    import matplotlib.cm as cm
-    import matplotlib as mpl
-
-    # Set the text color to white
-    mpl.rcParams['text.color'] = 'white'
-
+def cluster(latent_vectors, cluster_method='dbscan', n_clusters=35):
     if cluster_method == 'kmeans':
         from sklearn.cluster import KMeans
         # Instantiate the k-means clurtering algorithm
@@ -222,26 +227,123 @@ def plot_cluster(latent_vectors, mydir, cluster_method='dbscan', n_clusters=35):
         dbscan = DBSCAN(eps=0.5, min_samples=5)
         cluster_labels = dbscan.fit_predict(latent_vectors)
         n_clusters = np.max(cluster_labels) # Lables go as 0, 1, 2, ... and -1 for unclustered
+    return cluster_labels
 
+def plot_cluster(latent_vectors, ltatent_tsne, cluster_labels, mydir):
+    import matplotlib.cm as cm
+    import matplotlib as mpl
+    from sklearn.manifold import TSNE
+
+    # Set the text color to white
+    mpl.rcParams['text.color'] = 'white'
+    
     tsne = TSNE(n_components=2, random_state=42)
     latent_tsne = tsne.fit_transform(latent_vectors)
 
     # Create a scatter plot
     fig, ax = plt.subplots(facecolor='black')
+    ax.set_axis_off()
 
     # Define a color map for the clusters
+    n_clusters = len(np.unique(cluster_labels))
     cmap = cm.get_cmap('tab20', n_clusters)
 
     # Scatter plot with points colored by cluster membership
     scatter = ax.scatter(latent_tsne[:, 0], latent_tsne[:, 1],
                          c=cluster_labels,
-                         cmap=cmap, s=10, alpha=0.5)
+                         cmap=cmap, s=2, alpha=0.8)
 
     # Create a color bar legend
     cbar = fig.colorbar(scatter)
-    ax.set_axis_off()
+    cbar.ax.yaxis.set_tick_params(color='white')  # Set color bar tick labels to white
+    # cbar.ax.yaxis.label.set_color('white')
+    cbar.ax.yaxis.set_ticklabels(cbar.ax.get_yticklabels(), color='white')
 
-    plt.savefig(mydir + 'tsne_cluster.png')
+    # Set color bar edge color to white
+    cbar.outline.set_edgecolor('white')
+
+    plt.savefig(mydir + 'tsne_cluster.png', dpi=100)
+    print('Saved '+mydir + 'tsne_cluster.png')
+    
+def movie_cluster(latent_vectors, latent_tsne, cluster_labels, mydir):
+    import matplotlib.cm as cm
+    import matplotlib as mpl
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.animation import FuncAnimation, FFMpegWriter
+    from sklearn.manifold import TSNE
+    
+    # Set the text color to white
+    mpl.rcParams['text.color'] = 'white'    
+    
+    # Apply t-SNE to reduce dimensionality to 3D
+    tsne = TSNE(n_components=3)
+    latent_tsne = tsne.fit_transform(latent_vectors)
+    
+    # Create a 3D scatter plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_axis_off()    
+    
+    # Set black background
+    ax.set_facecolor('black')
+
+    # Define a color map for the clusters
+    n_clusters = len(np.unique(cluster_labels))
+    cmap = cm.get_cmap('tab20', n_clusters)
+    
+    # Scatter plot with points colored by cluster membership
+    colors = np.random.rand(50, 3)
+    scatter = ax.scatter(latent_tsne[:, 0], latent_tsne[:, 1],
+                         latent_tsne[:, 2], c=cluster_labels,
+                         cmap=cmap, s=2, alpha=0.8)
+    
+    # Remove white border
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    # Remove tick marks
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+    
+    # Remove grid lines
+    ax.grid(False)    
+    
+    # Animation parameters
+    frames = 180  # Number of frames for the animation
+    elev_start = 30
+    elev_end = 60
+    azim_start = 0
+    azim_end = 100# 360
+    zoom_start = 0.8
+    zoom_end = 0.4
+    
+    # Update function for the animation
+    def update(frame):
+        elev = elev_start + (elev_end - elev_start) * frame / frames
+        azim = azim_start + (azim_end - azim_start) * frame / frames
+        zoom = zoom_start + (zoom_end - zoom_start) * frame / frames
+    
+        ax.view_init(elev, azim)
+        ax.dist *= zoom 
+    
+    # Create the animation
+    animation = FuncAnimation(fig, update, frames=frames)
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+    
+    # Set up saving options
+    save_as_mp4 = True  # Set to False if you want to save as a GIF instead
+    if save_as_mp4:
+        writer = FFMpegWriter(fps=30, metadata=dict(artist='Me')) 
+        filename = '3d_tsne_rotation.mp4'
+    else:
+        filename = '3d_tsne_rotation.gif'
+    
+    # Save the animation
+    animation.save(mydir+filename, writer=writer if save_as_mp4 else 'pillow')
+    print('Saved '+mydir+filename)
     
 
 def plot_anomaly(latent_vectors, mydir, n_neighbors=20):
@@ -269,4 +371,153 @@ def plot_anomaly(latent_vectors, mydir, n_neighbors=20):
     # Print the number of anomalies
     print("Number of anomalies detected:", num_anomalies)
 
+def movie_light_curves(light_curves, errors, mydir, total_frames=100):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.animation import FuncAnimation, FFMpegWriter
+    
+    # Assuming you have your light curves and errors stored in variables called 'light_curves' and 'errors'
+    # light_curves and errors should be 2D numpy arrays with the same shape
+    
+    plt.rcParams['lines.color'] = 'black'    
+    
+    # Define the number of rows and columns to display
+    num_rows = 20
+    num_cols = 10
+     
+    # Calculate the total number of frames and the maximum number of rows
+    # total_frames = light_curves.shape[0] - num_rows + 1
+    total_frames = total_frames
+    max_rows = min(num_rows, light_curves.shape[0])
+    
+    # Create a figure and axes for the animation
+    fig, ax = plt.subplots(num_rows, num_cols, figsize=(12, 8), tight_layout=True)
+    
 
+    # Update function for the animation
+    def update(frame):
+        # Calculate the starting and ending indices for the rows to display in this frame
+        start_row = frame
+        end_row = start_row + max_rows
+    
+        # Update the data for each subplot
+        for i in range(max_rows):
+            for j in range(num_cols):
+                idx = start_row + i
+                if idx < light_curves.shape[0]:
+                    x = np.arange(light_curves.shape[1])
+                    y = light_curves[idx]
+                    y_err = errors[idx]
+    
+                    # Clear the current plot
+                    ax[i, j].cla()
+    
+                    # Calculate the alpha value for the current row
+                    alpha = (i + 1) / max_rows
+    
+                    # Plot the light curve with error bars and set the alpha value
+                    ax[i, j].errorbar(x, y, yerr=y_err, fmt='-', color='black', alpha=alpha)
+    
+                    ax[i, j].set_xlim(0, light_curves.shape[1])
+                    ax[i, j].set_ylim(np.min(light_curves - errors), np.max(light_curves + errors))
+                    ax[i, j].set_axis_off()
+
+    # Create the animation
+    animation = FuncAnimation(fig, update, frames=total_frames, interval=300)
+
+    # Set up saving options
+    save_as_mp4 = True  # Set to False if you want to save as a GIF instead
+    if save_as_mp4:
+        filename = 'scrolling_light_curves.mp4'
+        writer = FFMpegWriter(fps=30, metadata=dict(artist='Me')) 
+    else:
+        filename = 'scrolling_light_curves.gif'
+    
+    # Save the animation as an MP4 file
+    animation.save(mydir+filename, writer='ffmpeg')#, dpi=200)
+    print('Saved '+mydir+filename)
+
+def movie_cluster_insets(light_curves, errors, latent_vectors, latent_tsne, cluster_labels, mydir):
+        
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib.animation import FuncAnimation
+    from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+    import matplotlib.cm as cm
+    
+    # Assuming you have your 3D t-SNE data stored in a variable called 'tsne_data'
+    # tsne_data should be a 2D numpy array of shape (n_samples, 3)
+    
+    # Assuming you have your light curves stored in a variable called 'light_curves'
+    # light_curves should be a 2D numpy array of shape (n_samples, n_time_points)
+    
+    # Assuming you have your cluster labels stored in a variable called 'cluster_labels'
+    # cluster_labels should be a 1D numpy array of shape (n_samples,)
+    
+    # Define a color map for the clusters
+    n_clusters = len(np.unique(cluster_labels))
+    cmap = cm.get_cmap('tab20', n_clusters)    
+    
+    # Create a figure and axes for the animation
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    # Initialize the scatter plot
+    scatter = ax.scatter(latent_tsne[:, 0], latent_tsne[:, 1], latent_tsne[:, 2], c=cluster_labels, cmap=cmap)
+    
+    # Animation parameters
+    frames = 10  # Number of frames for the animation
+    elev_start = 30
+    elev_end = 60
+    azim_start = 0
+    azim_end = 100# 360
+    zoom_start = 0.8
+    zoom_end = 0.4
+    
+    # Update function for the animation
+    def update(frame):
+        elev = elev_start + (elev_end - elev_start) * frame / frames
+        azim = azim_start + (azim_end - azim_start) * frame / frames
+        zoom = zoom_start + (zoom_end - zoom_start) * frame / frames
+    
+        ax.view_init(elev, azim)
+        ax.dist *= zoom 
+        
+    # Create the animation
+    animation = FuncAnimation(fig, update, frames=np.arange(0, frames, 2), interval=100)
+    
+    # Define the inset parameters
+    inset_size = 0.2  # Size of the inset as a fraction of the main plot size
+    inset_pad = 0.1  # Padding between the inset and the main plot
+
+    
+    # Function to create the inset axes for each point
+    def create_inset_axes(ax, light_curve, cluster_label):
+        inset_ax = inset_axes(ax, width=inset_size, height=inset_size, loc='upper left', borderpad=inset_pad)
+        inset_ax.plot(light_curve)
+    
+        # Set the border color based on the cluster label
+        border_color = cmap(cluster_label)
+        for spine in inset_ax.spines.values():
+            spine.set_edgecolor(border_color)
+    
+        inset_ax.set_axis_off()
+    
+    # Function to update the insets in each frame
+    def update_insets(frame):
+        for point, light_curve, cluster_label in zip(scatter.get_offsets(), light_curves, cluster_labels):
+            create_inset_axes(ax, light_curve, cluster_label)
+    
+    # Call the update_insets function once to initialize the insets
+    update_insets(0)
+    
+    # Create the animation
+    animation = FuncAnimation(fig, update, frames=np.arange(0, 360, 2), interval=100)
+    
+    # Set up saving options
+    filename = '3d_tsne_with_light_curves.mp4'
+    
+    # Save the animation as an MP4 file
+    animation.save(mydir+filename, writer='ffmpeg')
+        
--- testing.py
+++ testing.py
@@ -1,8 +1,21 @@
 from plot_functions import *
+import numpy as np
+
+mydir = '/Users/emma/Desktop/230708/'
+# mydir ='/scratch/echickle/foo/'
 
 # Generate some random data for demonstration
-latent_vectors = np.random.randn(10000, 10)
+n_lc = 1000
+latent_vectors = np.random.randn(n_lc, 10)
+light_curves = np.random.randn(n_lc, 300)
+errors = np.ones((n_lc,300))*0.1
+cluster_labels = np.random.choice(np.arange(35), n_lc)
+latent_tsne_2d = np.random.randn(n_lc,2)
+latent_tsne_3d = np.random.randn(n_lc,3)
 
-mydir ='/scratch/echickle/foo/'
-plot_cluster(latent_vectors, mydir, cluster_method='kmeans')
+# Plot
+# plot_cluster(latent_vectors, latent_tsne_2d, cluster_labels, mydir)
+# movie_cluster( latent_vectors, latent_tsne_3d, cluster_labels, mydir)
+# movie_light_curves(light_curves, errors, mydir)
+movie_cluster_insets(light_curves, errors, latent_vectors, latent_tsne_3d, cluster_labels, mydir)
 
